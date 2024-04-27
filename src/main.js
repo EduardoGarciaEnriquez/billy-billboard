5a943021b1afb8c3b328ac3b243271917ce6c29b
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2024-04-26 22:43:19 -0600

    ci: movie detail, search and categories.

--- src/main.js
+++ src/main.js
@@ -6,14 +6,16 @@ const api = axios.create({
 const url = "https://api.themoviedb.org/3/";
 
 //get trending movies list
-async function getPopularMoviesPreview() {
-  const { data } = await api(url + "movie/popular");
+async function getTrendingMoviesPreview() {
+  const { data } = await api(url + "trending/movie/day");
 
   const moviesList = data.results;
 
   const moviesListContainer = document.querySelector(
     "#trendingPreview .trendingPreview-movieList"
   );
+
+  moviesListContainer.innerHTML = "";
 
   moviesList.forEach((item) => {
     appendMovieItem(item, moviesListContainer);
@@ -30,14 +32,101 @@ async function getCategoriesPreview() {
     "#categoriesPreview .categoriesPreview-list"
   );
 
+  genresListContainer.innerHTML = "";
+
   genres.forEach((item) => {
     appendGenreItem(item, genresListContainer);
   });
 }
 
+//get movies by genre list
+async function getMoviesByGenre({ id, name }) {
+  const { data } = await api(url + "discover/movie?with_genres=" + id);
+
+  const moviesList = data.results;
+
+  headerCategoryTitle.innerHTML = name.replace(/%20/g, " ");
+
+  genericSection.innerHTML = "";
+
+  moviesList.forEach((item) => {
+    appendMovieItem(item, genericSection);
+  });
+}
+
+//get movies by search result
+async function getMoviesBySearch(search) {
+  const { data } = await api(url + "search/movie?query=" + search);
+
+  const moviesList = data.results;
+
+  headerCategoryTitle.innerHTML = `results for "${search.replace(
+    /%20/g,
+    " "
+  )}"`;
+
+  genericSection.innerHTML = "";
+
+  moviesList.forEach((item) => {
+    appendMovieItem(item, genericSection);
+  });
+}
+
+//get movies by search result
+async function getTrendingMovies() {
+  const { data } = await api(url + "trending/movie/day");
+
+  const moviesList = data.results;
+
+  headerCategoryTitle.innerHTML = "Trending movies";
+
+  genericSection.innerHTML = "";
+
+  moviesList.forEach((item) => {
+    appendMovieItem(item, genericSection);
+  });
+}
+
+//get movies by search result
+async function getMovieDetail(id) {
+  const { data } = await api(url + "movie/" + id);
+
+  const { title, genres, poster_path, vote_average, overview } = data;
+
+  movieDetailTitle.innerHTML = title;
+  movieDetailScore.innerHTML = vote_average.toFixed(1);
+  movieDetailDescription.innerHTML = overview;
+  headerSection.style.background = `linear-gradient(
+    180deg,
+    rgba(0, 0, 0, 0.35) 19.27%,
+    rgba(0, 0, 0, 0) 29.17%
+  ),
+  url("https://image.tmdb.org//t/p/w500${poster_path}")`
+  movieDetailCategoriesList.innerHTML = "";
+
+  genres.forEach((item) => {
+    appendGenreItem(item, movieDetailCategoriesList);
+  });
+
+  getRelatedMovies(id);
+}
+
+//get related movies
+async function getRelatedMovies(id) {
+  const { data } = await api(`${url}movie/${id}/similar`);
+
+  const relatedMovies = data.results;
+
+  relatedMoviesContainer.innerHTML = "";
+
+  relatedMovies.forEach((item) => {
+    appendMovieItem(item, relatedMoviesContainer);
+  });
+}
+
 //append movie item
 function appendMovieItem(item, moviesListContainer) {
-  const { title, poster_path: src } = item;
+  const { title, poster_path: src, id } = item;
 
   const movieItemContainer = document.createElement("div");
   const image = document.createElement("img");
@@ -50,6 +139,11 @@ function appendMovieItem(item, moviesListContainer) {
 
   movieItemContainer.appendChild(image);
   moviesListContainer.appendChild(movieItemContainer);
+
+  movieItemContainer.addEventListener("click", (e) => {
+    e.preventDefault();
+    location.hash = `movie=${title}-${id}`;
+  });
 }
 
 //append genre item
@@ -67,7 +161,9 @@ function appendGenreItem(item, genresListContainer) {
 
   genreItemContainer.appendChild(title);
   genresListContainer.appendChild(genreItemContainer);
-}
 
-getPopularMoviesPreview();
-getCategoriesPreview();
+  title.addEventListener("click", (e) => {
+    e.preventDefault();
+    location.hash = `genre=${name}-${id}`;
+  });
+}
---
+++ src/navigation.js
@@ -0,0 +1,120 @@
+window.addEventListener("DOMContentLoaded", navigator, false);
+window.addEventListener("hashchange", navigator, false);
+
+searchFormBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let inputValue = searchFormInput.value;
+  if (inputValue) {
+    location.hash = "search=" + searchFormInput.value;
+  }
+});
+
+arrowBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  location.hash = window.history.back();
+});
+
+trendingBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  location.hash = "trending";
+});
+
+function navigator() {
+  window.scrollTo(0, 0);
+  if (location.hash.startsWith("#search")) {
+    searchPage();
+  } else if (location.hash.startsWith("#movie")) {
+    movieDetail();
+  } else if (location.hash.startsWith("#genre")) {
+    genrePage();
+  } else if (location.hash.startsWith("#trending")) {
+    trendingPage();
+  } else {
+    home();
+  }
+}
+
+function home() {
+  getTrendingMoviesPreview();
+  getCategoriesPreview();
+
+  headerSection.style.background = "";
+  headerSection.classList.remove("header-container--long");
+  arrowBtn.classList.add("inactive");
+  arrowBtn.classList.remove("header-arrow--white");
+  headerTitle.classList.remove("inactive");
+  headerCategoryTitle.classList.add("inactive");
+  searchForm.classList.remove("inactive");
+  trendingPreviewSection.classList.remove("inactive");
+  categoriesPreviewSection.classList.remove("inactive");
+  genericSection.classList.add("inactive");
+  movieDetailSection.classList.add("inactive");
+}
+
+function searchPage() {
+  headerSection.style.background = "";
+  headerSection.classList.remove("header-container--long");
+  arrowBtn.classList.remove("inactive");
+  arrowBtn.classList.remove("header-arrow--white");
+  headerTitle.classList.add("inactive");
+  headerCategoryTitle.classList.remove("inactive");
+  searchForm.classList.remove("inactive");
+  trendingPreviewSection.classList.add("inactive");
+  categoriesPreviewSection.classList.add("inactive");
+  genericSection.classList.remove("inactive");
+  movieDetailSection.classList.add("inactive");
+
+  let search = location.hash.split("=")[1];
+  getMoviesBySearch(search);
+}
+
+function movieDetail() {
+  headerSection.style.background = "white";
+  headerSection.classList.add("header-container--long");
+  arrowBtn.classList.remove("inactive");
+  arrowBtn.classList.add("header-arrow--white");
+  headerTitle.classList.add("inactive");
+  headerCategoryTitle.classList.add("inactive");
+  searchForm.classList.add("inactive");
+  trendingPreviewSection.classList.add("inactive");
+  categoriesPreviewSection.classList.add("inactive");
+  genericSection.classList.add("inactive");
+  movieDetailSection.classList.remove("inactive");
+
+  let id = location.hash.split("-")[1];
+  getMovieDetail(id);
+}
+
+function genrePage() {
+  headerSection.style.background = "";
+  headerSection.classList.remove("header-container--long");
+  arrowBtn.classList.remove("inactive");
+  arrowBtn.classList.remove("header-arrow--white");
+  headerTitle.classList.add("inactive");
+  headerCategoryTitle.classList.remove("inactive");
+  searchForm.classList.add("inactive");
+  trendingPreviewSection.classList.add("inactive");
+  categoriesPreviewSection.classList.add("inactive");
+  genericSection.classList.remove("inactive");
+  movieDetailSection.classList.add("inactive");
+
+  let genre = location.hash.split("=")[1];
+  let [name, id] = genre.split("-");
+  getMoviesByGenre({ name, id });
+}
+
+function trendingPage() {
+  headerSection.style.background = "";
+  headerSection.classList.remove("header-container--long");
+  arrowBtn.classList.remove("inactive");
+  arrowBtn.classList.remove("header-arrow--white");
+  headerTitle.classList.add("inactive");
+  headerCategoryTitle.classList.remove("inactive");
+  searchForm.classList.add("inactive");
+  trendingPreviewSection.classList.add("inactive");
+  categoriesPreviewSection.classList.add("inactive");
+  genericSection.classList.remove("inactive");
+  movieDetailSection.classList.add("inactive");
+
+  getTrendingMovies()
+}
